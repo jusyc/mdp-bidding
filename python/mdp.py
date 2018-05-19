8752eb4032b21f1aedb2a624e5f335c4d93aa469
ahenzinger <ahenzinger@gmail.com>
2018-05-18 21:55:12 -0700

    new stuff

--- python/mdp.py
+++ python/mdp.py
@@ -15,25 +15,26 @@ class BiddingMDP(util.MDP):
     #STATE = (#bids so far, #bids remaining, budget remaining, ad features, pctr, #impressions, amount spent)
     return (0, self.N, self.B, self.ads[0], self.pctrs[0], 0, 0)
 
+
   def actions(self, state):
     # n, b, ad, pctr, imps, clicks, cost = state
-    return [x for x in range(maxBidPrice+1) if x <= self.maxBidPrice]
+    return [x for x in range(min(maxBidPrice, self.B) + 1)]
 
   def succAndProbReward(self, state, action):
     i, n, b, ad, pctr, imps, cost = state
 
-    if n == 0:
+    if (n == 0) or (b == 0): # Reached end state
       return []
 
     click = self.clicks[i]
     winprice = self.clicks[i]
 
-    if action >= winprice:
+    if action >= winprice: # Won last auction
       newState = (i+1, n-1, b-winprice, ads[i+1], pctrs[i+1], imps+1, cost+winprice)
       return[(newState, 1., click)]
-    else:
-      newState = (i+1, n-1, b, ads[i+1], pctrs[i+1], imps, clicks, cost)
+    else: # Lost last auction
+      newState = (i+1, n-1, b, ads[i+1], pctrs[i+1], imps, cost)
       return[(newState, 1., 0)]
 
   def discount(self):
-    return 1
+    return 1.0
---
+++ python/qlearning_mdp.py
@@ -0,0 +1,54 @@
+# Given a policy, run the MDP on the policy over a given ad campaign
+# Code based off of CMDP paper code, Q-Learning code from class
+
+import mdp
+import numpy as np
+from config import *
+from generate_pdfs import *
+
+if __name__ == '__main__':
+  camp = campaigns[0]
+  dataPath = outPath + camp + "/test/"
+
+  auction = {}
+  auction["pctr"] = np.load(dataPath + "pCTR.npy")
+  auction["prices"] = np.load(dataPath + "prices.npy")
+  auction["clicks"] = np.load(dataPath + "clicks.npy")
+
+  ) 
+
+  #calculating distributions of CTR and market price
+  bins = makeBins()
+  pctr_pdf = get_pctr_pdf(auction["pctr"], bins)
+
+  m_pdf = get_m_pdf(auction["prices"], auction["pctr"], pctr_pdf, bins)
+
+  #create baseline policy
+  policy_name = "qlearning"
+  baseline = medianMarketPricePolicy(m_pdf)
+
+  logging = True
+
+  #calulating budget
+  c0_list = ["32", "16", "8", "4"]
+  for c0_str in c0_list:
+    c0 = 1/float(c0_str)
+    N = len(auction["clicks"])
+    B = calculateBudget(c0, N)
+
+    #setting up logging
+    bid_log = []
+
+    #make MDP
+    mdp = BiddingMDP(N=N, B=)  
+
+    #test baseline policy
+    run(auction, N=N, policy=baseline, budget=B, pctr_pdf=pctr_pdf, pctr_bins=bins,
+        bid_log=bid_log, logging=logging, verbose=False)
+
+    if logging:
+      with open(logPath + camp + "/" + policy_name + "/" + c0_str + ".txt", "w") as f:
+        for line in bid_log:
+          f.write("{}\n".format(line))
+
+
